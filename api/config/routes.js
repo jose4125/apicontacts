cfa8b2e67099ff5c0209300d369be0fa5d7fcde1
jose david lombana <jose4125@gmail.com>
2013-09-12 10:37:37 -0500

    put api in the routes

--- api/config/routes.js
+++ api/config/routes.js
@@ -4,18 +4,18 @@ module.exports = function( app ){
 	var contacts = require( '../app/controllers/contacts' );
 	
 	//home route
-	app.get( '/', contacts.findAll );
+	app.get( '/api/', contacts.findAll );
 
 	//find a contact route
-	app.get( '/contact/:id', contacts.findById );
+	app.get( '/api/contact/:id', contacts.findById );
 
 	//add a contact route
-	app.post( '/add', contacts.addContact );
+	app.post( '/api/add', contacts.addContact );
 
 	//update a contact route
-	app.put( '/contact/:id/edit', contacts.editContact );
+	app.put( '/api/contact/:id/edit', contacts.editContact );
 
 	//update a contact route
-	app.delete( '/contact/:id/delete', contacts.deleteContact );
+	app.delete( '/api/contact/:id/delete', contacts.deleteContact );
 
 };
