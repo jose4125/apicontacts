df60373869b788b1064681fdb62934c4ebf6d35a
jose david lombana <jose4125@gmail.com>
2013-09-11 19:19:50 -0500

    remove some files

--- config/routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app){
-
-	//all contacts route
-	var contacts = require('../app/controllers/contacts');
-	app.get('/', contacts.index);
-
-	//find contact route
-	app.get('/:id', contacts.find);
-};
