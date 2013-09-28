53ad7a4b2bbb6f2709bcc68fd98706e73a568e7a
jose david lombana <jose4125@gmail.com>
2013-09-28 01:16:28 -0500

    router add contact withouth zombie view add contact - showed much modals

--- web/app/scripts/routes/main.js
+++ web/app/scripts/routes/main.js
@@ -18,21 +18,25 @@ define([
 
         index: function(){
         	console.log( 'index route' );
-            var contacts = new ContactsCollection();
+            this.contacts = new ContactsCollection();
 
             var addModal = $( '#addContact' );
             var addHtml = addModal.html();
+            var self = this;
 
             if ( addHtml ){
                 addModal.modal( 'hide' );
+                addModal.on( 'hidden.bs.modal', function(){
+                    this.remove();
+                } );
+
             }else{
 
-            contacts.fetch().then(function(){
-                console.log( 'contacts', contacts );
-                var contactsView = new ContactsColView( {collection: contacts} );
-                var homeView = new HomeView( {collection: contacts} );
+            this.contacts.fetch().then(function(){
+                console.log( 'contacts', self.contacts );
+                var contactsView = new ContactsColView( {collection: self.contacts} );
                 contactsView.render();
-                homeView.render().el;
+                self.homeFunc( 'index' );
             });
             }
 
@@ -40,13 +44,25 @@ define([
 
         },
 
+        homeFunc: function( path ){
+            if (! this.homeView) {
+                this.homeView = new HomeView( {collection: this.contacts} );
+            }
+            if ( path !== 'index'){
+                this.homeView.addContactModal()
+            }
+                  
+        },
+
         addContact: function(){
             console.log( 'add contact router' );            
+            var self = this;
             var contacts = new ContactsCollection();
             contacts.fetch().then(function(){
                 console.log( 'contacts', contacts );
-                var homeView = new HomeView( {collection: contacts} );
-                homeView.addContactModal();
+                self.homeFunc();
+
+
             });
         }
 
